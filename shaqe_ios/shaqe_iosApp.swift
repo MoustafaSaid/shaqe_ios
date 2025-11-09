18453d773c9b7739c1ff45ef062c35043134b4d3
moustafa said salha <100960738+MoustafaSaid@users.noreply.github.com>
2025-11-09 10:26:51 +0200

    Initial Commit

---
+++ shaqe_ios/shaqe_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  shaqe_iosApp.swift
+//  shaqe_ios
+//
+//  Created by moustafa on 09/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct shaqe_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
